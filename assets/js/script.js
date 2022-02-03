47366f544c31718a982a83caeb48949fcc99c9ee
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-02-03 20:04:16 +0500

    add code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const $button  = document.querySelector('#sidebar-toggle');
+const $wrapper = document.querySelector('#wrapper');
+
+$button.addEventListener('click', (e) => {
+  e.preventDefault();
+  $wrapper.classList.toggle('toggled');
+});
+const $buttonb  = document.querySelector('#sidebar-toggle-inner');
+
+$buttonb.addEventListener('click', (e) => {
+  e.preventDefault();
+  $wrapper.classList.toggle('toggled');
+});
+
+
+
